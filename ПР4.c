4bbbd09d5a0ab07e819ae48356b2cfbdf6e20f1f
PeterAnt08 <kn24-p.antoniuk@nubip.edu.ua>
2025-07-06 22:23:17 +0300

    ПР4 Антонюк

---
+++ ПР4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+    scanf("%d", &n);
+    for (int m = 1; m < n; m++) {
+        if (n % m == n / m)
+            count++;
+    }
+    printf("Кількість рівних дільників числа %d: %d\n", n, count);
+    return 0;
+}
