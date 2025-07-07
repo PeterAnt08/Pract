80fcb73b6195baee93360002499a7084af679ebc
PeterAnt08 <kn24-p.antoniuk@nubip.edu.ua>
2025-07-07 14:15:15 +0300

    Update ПР5.c

--- ПР5.c
+++ ПР5.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MOD 12345
 
 int main() {
     int n;
+    printf("Введіть довжину послідовності n: ");
     scanf("%d", &n);
-    int dp[n+1];
-    dp[0] = 1;
-    dp[1] = 2;
-    dp[2] = 4;
-    for (int i = 3; i <= n; i++) {
-        dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % MOD;
+
+    if (n == 0) {
+        printf("0\n");
+        return 0;
     }
-    printf("Кількість шуканих послідовностей: %d\n", dp[n]);
+
+    int dp[n + 1][3];
+    memset(dp, 0, sizeof(dp));
+
+    dp[1][0] = 1; 
+    dp[1][1] = 1; 
+    dp[1][2] = 0; 
+
+    for (int i = 2; i <= n; i++) {
+        dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % MOD;
+
+        dp[i][1] = dp[i-1][0] % MOD;
+        dp[i][2] = dp[i-1][1] % MOD;
+    }
+
+    int result = (dp[n][0] + dp[n][1] + dp[n][2]) % MOD;
+    printf("Кількість шуканих послідовностей: %d\n", result);
+
     return 0;
 }
