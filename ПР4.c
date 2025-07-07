ec51239342126a97e9e768fbdc7725de7a7e4cd3
PeterAnt08 <kn24-p.antoniuk@nubip.edu.ua>
2025-07-07 14:11:08 +0300

    ПР4.c

--- ПР4.c
+++ ПР4.c
@@ -2,11 +2,15 @@
 
 int main() {
     int n, count = 0;
+    printf("Введіть натуральне число n: ");
     scanf("%d", &n);
-    for (int m = 1; m < n; m++) {
-        if (n % m == n / m)
+
+    for (int m = 1; m <= n; m++) {
+        if (n / m == n % m) {
             count++;
+        }
     }
+
     printf("Кількість рівних дільників числа %d: %d\n", n, count);
     return 0;
 }
